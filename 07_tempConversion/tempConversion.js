648dcbab05148bfb1f8476700ba7c4238d9cd755
Eric Had Arach <hadarachericj@gmail.com>
2023-07-18 15:43:20 -0300

    Do exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+// Formula to convert from Celsius to Fahrenheit --> (tempºC × 9/5) + 32 = 32 °F
+// Formula to convert from Fahrenheit to Celsius --> (tempºF - 32) * 5/9
+
+const convertToCelsius = function(fahrenheit) {
+  return (Math.round(((fahrenheit - 32) * 5/9) * 10)) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return (Math.round(((celsius * 9/5) + 32) * 10)) / 10;
 };
+
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
