8e512bdebecd9c02121821bbeccab113756035d0
Eric Had Arach <hadarachericj@gmail.com>
2023-07-25 15:16:22 -0300

    Do exercise 8 - calculator.

--- odin-project/08_calculator/calculator.js
+++ odin-project/08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+  return arrayOfNumbers.reduce((accumulator, element) => {
+    return accumulator + element;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  let result = 1;
+  return numbers.reduce((accumulator, element) => {
+    return accumulator * element;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(basis, exponent) {
+	let result = basis;
+  for (let i = 2; i <= exponent; i++) {
+    result *= basis;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num-1);
 };
 
 // Do not edit below this line
@@ -31,3 +43,4 @@ module.exports = {
   power,
   factorial
 };
+
