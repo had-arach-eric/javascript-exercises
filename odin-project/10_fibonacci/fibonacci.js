7da8c7a6442eb8802aa46be9d1edfc1d1c0ace4a
Eric Had Arach <hadarachericj@gmail.com>
2023-07-25 21:38:53 -0300

    Do exercise 10.

--- odin-project/10_fibonacci/fibonacci.js
+++ odin-project/10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 
+const fibonacci = function(position) {
+  if (+position < 0) {
+    return "OOPS";
+  }
+  const succession = [0, 1];
+  for (let i = 2; i <= +position; i++) {
+    succession.push(succession[i-1] + succession[i-2]);
+  }
+  return succession[+position];
 };
 
 // Do not edit below this line
