40b8f32899607e5e95e9eae8e0f7116e4592ef0e
Eric Had Arach <hadarachericj@gmail.com>
2023-07-25 21:26:04 -0300

    Do exercise 9.

--- odin-project/09_palindromes/palindromes.js
+++ odin-project/09_palindromes/palindromes.js
@@ -1,4 +1,30 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const originalArrayText = [];
+  let newArrayText = [];
+  let reverseArray = [];
+
+  for (let i = 0; i < text.length; i++) {
+    originalArrayText.push(text[i].toLowerCase());
+  }
+
+  newArrayText = originalArrayText.filter(element => {
+    return (
+      (element.charCodeAt(0) >= 48 && element.charCodeAt(0) <= 57) || 
+      (element.charCodeAt(0) >= 97 && element.charCodeAt(0) <= 122)
+    );
+  });
+
+  for (let i = 0; i < newArrayText.length; i++) {
+    reverseArray.push(newArrayText[i]);
+  }
+  reverseArray.reverse();
+
+  for (let i = 0; i < newArrayText.length; i++) {
+    if (newArrayText[i] !== reverseArray[i]) {
+      return false;
+    }
+  }
+  return true;
 
 };
 
